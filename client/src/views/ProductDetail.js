69646353965b289a0557452ba0793a75df0f612b
Emelie Persson <emelie.alltek@hotmail.com>
2023-03-22 20:40:39 +0100

    Rating styling

--- client/src/views/ProductDetail.js
+++ client/src/views/ProductDetail.js
@@ -1,12 +1,18 @@
 import { useParams } from "react-router-dom";
 import Image from "react-bootstrap/Image";
 import { BsStar, BsStarFill } from "react-icons/bs";
-import { getOne, getProductRatings, setProductRating } from "../services/productService";
+import {
+  getOne,
+  getProductRatings,
+  setProductRating,
+} from "../services/productService";
 import { useState, useEffect, useContext } from "react";
 import { CartContext } from "../App";
-import Dropdown from "react-bootstrap/Dropdown"
+import Dropdown from "react-bootstrap/Dropdown";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/esm/Col";
 
 function ProductDetail() {
   const params = useParams();
@@ -20,7 +26,7 @@ function ProductDetail() {
   useEffect(() => {
     getOne(id).then((result) => {
       setProduct(result);
-      if(result.ratings.length > 0) {
+      if (result.ratings.length > 0) {
         setRating(result.ratings[0].AverageRating);
       }
     });
@@ -32,13 +38,13 @@ function ProductDetail() {
   }
 
   useEffect(() => {
-    getProductRatings(id).then(result => {
-      console.log(result)
+    getProductRatings(id).then((result) => {
+      console.log(result);
       setRatings(result);
-    })
-  }, [id])
+    });
+  }, [id]);
 
-  function getRating() {
+  function getRating(rating) {
     let stars = [];
 
     for (let i = 1; i <= 5; i++) {
@@ -48,7 +54,7 @@ function ProductDetail() {
         stars.push(<BsStar />);
       }
     }
-  
+
     return stars;
   }
 
@@ -58,80 +64,93 @@ function ProductDetail() {
 
   return (
     <>
-    <Container className="rounded mt-2 mb-5">
-      <Image
-        rounded="true"
-        height="300px"
-        src={product.imageUrl}
-      ></Image>
-      <h3>{product.title}</h3>
-      <p>{product.description}</p>
-      <p>{product.price} kr</p>
-      <p>{getRating()}</p>
-      
-      <Button variant="success" onClick={addProductToCart}>
-        Lägg till i kundvagn
-      </Button>
+      <Container className="rounded mt-2 mb-5">
+        <Row>
+          <Col>
+          <Image rounded="true" height="300px" src={product.imageUrl}></Image>
+          <h3>{product.title}</h3>
+          <p>{product.description}</p>
+          <p>{product.price} kr</p>
+          <p>{getRating(rating)}</p>
 
-      <h4>Lämna betyg på produkt</h4>
-      <Dropdown>
-        <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-          Betyg
-        </Dropdown.Toggle>
+          <Button variant="success" onClick={addProductToCart}>
+            Lägg till i kundvagn
+          </Button>
+          </Col>
 
-        <Dropdown.Menu>
-          <Dropdown.Item onClick={() => postRating(5)}>
-            <BsStarFill></BsStarFill>
-            <BsStarFill></BsStarFill>
-            <BsStarFill></BsStarFill>
-            <BsStarFill></BsStarFill>
-            <BsStarFill></BsStarFill>
-          </Dropdown.Item>
-          <Dropdown.Item onClick={() => postRating(4)}>
-            <BsStarFill></BsStarFill>
-            <BsStarFill></BsStarFill>
-            <BsStarFill></BsStarFill>
-            <BsStarFill></BsStarFill>
-            <BsStar></BsStar>
-          </Dropdown.Item>
-          <Dropdown.Item onClick={() => postRating(3)}>
-            <BsStarFill></BsStarFill>
-            <BsStarFill></BsStarFill>
-            <BsStarFill></BsStarFill>
-            <BsStar></BsStar>
-            <BsStar></BsStar>
-          </Dropdown.Item>
-          <Dropdown.Item onClick={() => postRating(2)}>
-            <BsStarFill></BsStarFill>
-            <BsStarFill></BsStarFill>
-            <BsStar></BsStar>
-            <BsStar></BsStar>
-            <BsStar></BsStar>
-          </Dropdown.Item>
-          <Dropdown.Item onClick={() => postRating(1)}>
-            <BsStarFill></BsStarFill>
-            <BsStar></BsStar>
-            <BsStar></BsStar>
-            <BsStar></BsStar>
-            <BsStar></BsStar>
-          </Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+          <Col>
+          <h4>Lämna betyg på produkt</h4>
+          <Dropdown>
+            <Dropdown.Toggle variant="light" id="dropdown-basic">
+              Betyg
+            </Dropdown.Toggle>
 
-      <p>Tidigare betyg</p>
-      {ratings &&
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => postRating(5)}>
+                <BsStarFill></BsStarFill>
+                <BsStarFill></BsStarFill>
+                <BsStarFill></BsStarFill>
+                <BsStarFill></BsStarFill>
+                <BsStarFill></BsStarFill>
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => postRating(4)}>
+                <BsStarFill></BsStarFill>
+                <BsStarFill></BsStarFill>
+                <BsStarFill></BsStarFill>
+                <BsStarFill></BsStarFill>
+                <BsStar></BsStar>
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => postRating(3)}>
+                <BsStarFill></BsStarFill>
+                <BsStarFill></BsStarFill>
+                <BsStarFill></BsStarFill>
+                <BsStar></BsStar>
+                <BsStar></BsStar>
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => postRating(2)}>
+                <BsStarFill></BsStarFill>
+                <BsStarFill></BsStarFill>
+                <BsStar></BsStar>
+                <BsStar></BsStar>
+                <BsStar></BsStar>
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => postRating(1)}>
+                <BsStarFill></BsStarFill>
+                <BsStar></BsStar>
+                <BsStar></BsStar>
+                <BsStar></BsStar>
+                <BsStar></BsStar>
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <h4>Tidigare betyg</h4>
+          {ratings &&
             ratings.map((r) => {
               return (
                 <>
-                  <p><b>{r.rating}</b></p>
-                
+                  <p></p>
+                  <p>
+                    {formatDate(r.createdAt)} <b>{getRating(r.rating)}</b>
+                  </p>
                 </>
               );
             })}
-            </Container>
+            </Col>
+        </Row>
+      </Container>
     </>
   );
-}
 
+  function formatDate(date) {
+    var result = "";
+    const d = new Date(date);
+    result += d.toLocaleDateString("sv-SE");
+    result += " ";
+    result += d.toLocaleTimeString("sv-SE");
+
+    return result;
+  }
+}
 
 export default ProductDetail;
